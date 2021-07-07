33065b7c6601218c765256b03de924e568f66bac
ian-kevin126 <ian_kevin126@163.com>
2021-07-07 17:06:47 +0800

    feat: update double_linked list

--- src/4_LinkedList/doubly_linked_list.js
+++ src/4_LinkedList/doubly_linked_list.js
@@ -2,154 +2,242 @@
  * 双向链表：单向链表中的每一个元素只有一个next指针，用来指向下一个节点，这使得我们只能从链表的头部开始遍历整个链表，任何一个节点只能找到它的下一个节点，而不能找到它的上一个节点。
  * 这时候就出现了双向链表，双向链表中的每一个元素拥有两个指针，一个用来指向下一个节点，一个用来指向上一个节点。这样，我们就不仅可以从头部遍历整个链表，还能从尾部遍历整个链表。
  */
-import LinkedList from "./singly_linked_list";
-
-// 双向链表的节点类（继承单向链表的节点类）
-// class DoublyNode extends Node {
-//   constructor(element) {
-//     super(element);
-//     this.prev = null;
-//   }
-// }
-
-// const Node = function (element = null, next = null, prev = null) {
-//   this.element = element;
-//   this.next = next;
-//   this.prev = prev;
-// };
-
-class Node {
-  constructor(element = null, next = null, prev = null) {
-    this.element = element;
-    this.next = next;
-    this.prev = prev;
-  }
-}
-
-class DoubleLinkedList extends LinkedList {
-  constructor() {
-    super();
-    this.tail = null;
-  }
-
-  // 重写append方法，往双向链表的尾部增加一个新的元素
-  append(element) {
-    const node = new Node(element);
-    if (this.head === null) {
-      this.head = node;
-      this.tail = node;
-    } else {
-      this.tail.next = node;
-      node.prev = this.tail;
-      this.tail = node;
+let DoublyLinkedList = (function () {
+  class Node {
+    constructor(element) {
+      this.element = element;
+      this.next = null;
+      this.prev = null; //NEW
     }
-    this.length++;
   }
 
-  // 执行位置插入元素
-  insert(element, position) {
-    if (position < 0 || position > this.length) return false;
+  const length = new WeakMap();
+  const head = new WeakMap();
+  const tail = new WeakMap(); // NEW
 
-    const node = new Node(element);
+  class DoublyLinkedList {
+    constructor() {
+      length.set(this, 0);
+      head.set(this, null);
+      tail.set(this, null);
+    }
 
-    // 在开头插入元素
-    if (position === 0) {
-      if (this.head === null) {
-        this.head = node;
-        this.tail = node;
+    append(element) {
+      let node = new Node(element),
+        current,
+        _tail;
+
+      if (this.getHead() === null) {
+        //first node on list
+        head.set(this, node);
+        tail.set(this, node); //NEW
       } else {
-        node.next = this.head;
-        this.head.prev = node;
-        this.head = node;
+        //attach to the tail node //NEW
+        _tail = this.getTail();
+        _tail.next = node;
+        node.prev = _tail;
+        tail.set(this, node);
       }
-    } else if (position === this.length) {
-      // 在最后一个位置插入元素
-      this.tail.next = node;
-      node.prev = this.tail;
-      this.tail = node;
-    } else {
-      // 在中间的位置插入元素
-      let current = this.head,
-        previous,
-        index = 0;
-      while (index++ < position) {
-        previous = current;
-        current = current.next;
-      }
-      previous.next = node;
-      node.prev = previous;
-      node.next = current;
-      current.prev = node;
-    }
-    this.length++;
-    return true;
-  }
 
-  // 删除指定位置的节点
-  removeAt(position) {
-    // 1、position 越界判断
-    if (position < 0 || position > this.length - 1) return null;
-    // 2、根据不同情况删除元素
-    let current = this.head;
-    if (position === 0) {
-      // 删除第一个节点的情况
-      if (this.length === 1) {
-        // 链表内只有一个节点的情况
-        this.head = null;
-        this.tail = null;
+      //update size of list
+      let l = this.size();
+      l++;
+      length.set(this, l);
+    }
+
+    insert(position, element) {
+      //check for out-of-bounds values
+      if (position >= 0 && position <= this.size()) {
+        let node = new Node(element),
+          current = this.getHead(),
+          previous,
+          index = 0;
+
+        if (position === 0) {
+          //add on first position
+
+          if (!this.getHead()) {
+            //NEW
+            head.set(this, node);
+            tail.set(this, node);
+          } else {
+            node.next = current;
+            current.prev = node; //NEW {1}
+            head.set(this, node);
+          }
+        } else if (position === this.size()) {
+          //last item //NEW
+
+          current = tail; // {2}
+          current.next = node;
+          node.prev = current;
+          tail.set(this, node);
+        } else {
+          while (index++ < position) {
+            //{3}
+            previous = current;
+            current = current.next;
+          }
+          node.next = current;
+          previous.next = node;
+
+          current.prev = node; //NEW
+          node.prev = previous; //NEW
+        }
+
+        //update size of list
+        let l = this.size();
+        l++;
+        length.set(this, l);
+
+        return true;
       } else {
-        // 链表内有多个节点的情况
-        this.head = this.head.next;
-        this.head.prev = null;
+        return false;
       }
-    } else if (position === this.length - 1) {
-      // 删除最后一个节点的情况
-      current = this.tail;
-      this.tail.prev.next = null;
-      this.tail = this.tail.prev;
-    } else {
-      // 删除 0 ~ this.length - 1 里面节点的情况
-      let index = 0;
-      let previous = null;
-      while (index++ < position) {
-        previous = current;
+    }
+
+    removeAt(position) {
+      //check for out-of-bounds values
+      if (position > -1 && position < this.size()) {
+        let _head = this.getHead(),
+          _tail = this.getTail(),
+          current = _head,
+          previous,
+          index = 0;
+
+        //removing first item
+        if (position === 0) {
+          _head = current.next; // {1}
+
+          //if there is only one item, then we update tail as well //NEW
+          if (this.size() === 1) {
+            // {2}
+            _tail = null;
+          } else {
+            _head.prev = null; // {3}
+          }
+        } else if (position === this.size() - 1) {
+          //last item //NEW
+
+          current = _tail; // {4}
+          _tail = current.prev;
+          _tail.next = null;
+        } else {
+          while (index++ < position) {
+            // {5}
+
+            previous = current;
+            current = current.next;
+          }
+
+          //link previous with current's next - skip it to remove
+          previous.next = current.next; // {6}
+          current.next.prev = previous; //NEW
+        }
+
+        head.set(this, _head);
+        tail.set(this, _tail);
+
+        //update size of list
+        let l = this.size();
+        l--;
+        length.set(this, l);
+
+        return current.element;
+      } else {
+        return null;
+      }
+    }
+
+    remove(element) {
+      let index = this.indexOf(element);
+      return this.removeAt(index);
+    }
+
+    indexOf(element) {
+      let current = this.getHead(),
+        index = -1;
+
+      //check first item
+      if (element == current.element) {
+        return 0;
+      }
+
+      index++;
+
+      //check in the middle of the list
+      while (current.next) {
+        if (element == current.element) {
+          return index;
+        }
+
         current = current.next;
+        index++;
       }
-      previous.next = current.next;
-      current.next.perv = previous;
-    }
-    this.length--;
-    return current.element;
-  }
 
-  // 修改指定位置的节点
-  update(position, element) {
-    // 1、删除 position 位置的节点
-    const result = this.removeAt(position);
-    // 2、在 position 位置插入元素
-    this.insert(position, element);
-    return result;
-  }
+      //check last item
+      if (element == current.element) {
+        return index;
+      }
 
-  // 链表数据从前往后以字符串形式返回
-  forwardToString() {
-    let current = this.head;
-    let result = "";
-    while (current) {
-      result += current.element + "--";
-      current = current.next;
+      return -1;
     }
-    return result;
-  }
 
-  // 链表数据从后往前以字符串的形式返回
-  backwardString() {
-    let current = this.tail;
-    let result = "";
-    while (current) {
-      result += current.element + "--";
-      current = current.prev;
+    isEmpty() {
+      return this.size() === 0;
     }
-    return result;
+
+    size() {
+      return length.get(this);
+    }
+
+    toString() {
+      let current = this.getHead(),
+        s = current ? current.element : "";
+
+      while (current && current.next) {
+        current = current.next;
+        s += ", " + current.element;
+      }
+
+      return s;
+    }
+
+    inverseToString() {
+      let current = this.getTail(),
+        s = current ? current.element : "";
+
+      while (current && current.prev) {
+        current = current.prev;
+        s += ", " + current.element;
+      }
+
+      return s;
+    }
+
+    print() {
+      console.log(this.toString());
+    }
+
+    printInverse() {
+      console.log(this.inverseToString());
+    }
+
+    getHead() {
+      return head.get(this);
+    }
+
+    getTail() {
+      return tail.get(this);
+    }
   }
-}
+  return DoublyLinkedList;
+})();
+
+let list = new DoublyLinkedList();
+list.append(15);
+list.append(16); //15,16
+list.print(); //15,16
+list.printInverse(); //16, 15
+list.insert(0, 13); //13,15,16
+list.removeAt(0); //15,16
