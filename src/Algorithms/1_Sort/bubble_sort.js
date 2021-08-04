2f320021efbec031c93f660c7f6f6140fb2b8eed
ian_kevin126 <ian_kevin126@126.com>
2021-08-04 20:32:00 +0800

    feat: add bubble sort

--- src/Algorithms/1_Sort/bubble_sort.js
+++ src/Algorithms/1_Sort/bubble_sort.js
@@ -1,4 +1,50 @@
+import { shortArray, longArray } from '../data'
 /**
- * 冒泡排序
+ * 冒泡排序——基础版
  */
-console.log("sorttttttttttt");
+const bubble_sort_basical_edition = (arr) => {
+  const len = arr.length
+  if (len < 2) return arr
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = temp
+      }
+    }
+  }
+  return arr
+}
+
+/**
+ * 冒泡排序——进阶版
+ */
+const bubble_sort_advanced_edition = (arr) => {
+  const len = arr.length
+  let flag = false
+  if (len < 2) return arr
+  for (let i = 0; i < len; i++) {
+    flag = false // 提前退出冒泡循环的标志
+    for (let j = 0; j < len - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = temp
+        flag = true // 表示有数据交换
+      }
+    }
+    if (!flag) break // 没有数据交换，提前退出
+  }
+  return arr
+}
+
+console.time('基础版冒泡排序时间')
+console.log(bubble_sort_basical_edition(shortArray))
+// [0, 1, 2, 4, 5, 5, 6, 7, 9, 21, 34, 57, 78, 89, 89, 100, 123, 221, 453, 657]
+console.timeEnd('基础版冒泡排序时间') // 基础版冒泡排序时间: 312.9873046875 ms
+
+console.time('进阶版冒泡排序时间')
+console.log(bubble_sort_advanced_edition(shortArray))
+// [0, 1, 2, 4, 5, 5, 6, 7, 9, 21, 34, 57, 78, 89, 89, 100, 123, 221, 453, 657]
+console.timeEnd('进阶版冒泡排序时间') // 进阶版冒泡排序时间: 256.81591796875 ms
---
+++ src/Algorithms/data.js
@@ -0,0 +1,13 @@
+let longArray = []
+for (let n = 0; n < 10000; n++) {
+  longArray.push(Math.random())
+}
+
+const shortArray = [
+  1, 221, 453, 657, 57, 89, 0, 4, 2, 21, 5, 78, 9, 123, 34, 5, 6, 7, 89, 100,
+]
+
+export {
+  longArray,
+  shortArray
+}
