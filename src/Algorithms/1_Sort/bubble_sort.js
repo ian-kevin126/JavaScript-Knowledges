87d6f2005d5e6dd554fe24ad720cb8996774de44
liaojiayun <liaojiayun@moresales.cn>
2021-08-09 10:14:47 +0800

    test github

--- src/Algorithms/1_Sort/bubble_sort.js
+++ src/Algorithms/1_Sort/bubble_sort.js
@@ -12,7 +12,7 @@
  * https://juejin.cn/post/6932482325159067656
  */
 
-const { shortArray, longArray } = require('../data')
+const { shortArray, longArray } = require("../data");
 
 /**
  * 基础版
@@ -20,17 +20,17 @@ const { shortArray, longArray } = require('../data')
  * @returns
  */
 const bubble_sort_basical_edition = (arr) => {
-  const len = arr.length
-  if (len < 2) return arr
+  const len = arr.length;
+  if (len < 2) return arr;
   for (let i = 0; i < len; i++) {
     for (let j = 0; j < len - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
-        ;[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
       }
     }
   }
-  return arr
-}
+  return arr;
+};
 
 /**
  * 进阶版
@@ -38,50 +38,50 @@ const bubble_sort_basical_edition = (arr) => {
  * @returns
  */
 const bubble_sort_advanced_edition = (arr) => {
-  const len = arr.length
-  let flag = false
-  if (len < 2) return arr
+  const len = arr.length;
+  let flag = false;
+  if (len < 2) return arr;
   for (let i = 0; i < len; i++) {
-    flag = false // 提前退出冒泡循环的标志
+    flag = false; // 提前退出冒泡循环的标志
     for (let j = 0; j < len - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
-        ;[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
-        flag = true // 表示有数据交换
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        flag = true; // 表示有数据交换
       }
     }
-    if (!flag) break // 没有数据交换，提前退出
+    if (!flag) break; // 没有数据交换，提前退出
   }
-  return arr
-}
+  return arr;
+};
 
 const bubble_copy = (arr) => {
-  const array = [...arr]
-  const len = array.length
-  let flag = false
+  const array = [...arr];
+  const len = array.length;
+  let flag = false;
   for (let i = 0; i < len; i++) {
-    flag = false
+    flag = false;
     for (let j = 0; j < len - i - 1; j++) {
       if (array[j] > array[j + 1]) {
-        ;[array[j], array[j + 1]] = [array[j + 1], array[j]]
-        flag = true
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
+        flag = true;
       }
-      if (!flag) break
+      if (!flag) break;
     }
   }
-  return array
-}
+  return array;
+};
 
-console.time('基础版冒泡排序时间')
-console.log(bubble_sort_basical_edition(shortArray))
+console.time("基础版冒泡排序时间");
+console.log(bubble_sort_basical_edition(shortArray));
 // [0, 1, 2, 4, 5, 5, 6, 7, 9, 21, 34, 57, 78, 89, 89, 100, 123, 221, 453, 657]
-console.timeEnd('基础版冒泡排序时间') // 基础版冒泡排序时间: 312.9873046875 ms
+console.timeEnd("基础版冒泡排序时间"); // 基础版冒泡排序时间: 312.9873046875 ms
 
-console.time('进阶版冒泡排序时间')
-console.log(bubble_sort_advanced_edition(shortArray))
+console.time("进阶版冒泡排序时间");
+console.log(bubble_sort_advanced_edition(shortArray));
 // [0, 1, 2, 4, 5, 5, 6, 7, 9, 21, 34, 57, 78, 89, 89, 100, 123, 221, 453, 657]
-console.timeEnd('进阶版冒泡排序时间') // 进阶版冒泡排序时间: 256.81591796875 ms
+console.timeEnd("进阶版冒泡排序时间"); // 进阶版冒泡排序时间: 256.81591796875 ms
 
-console.time('练习')
-console.log(bubble_copy(shortArray))
+console.time("练习");
+console.log(bubble_copy(shortArray));
 // [0, 1, 2, 4, 5, 5, 6, 7, 9, 21, 34, 57, 78, 89, 89, 100, 123, 221, 453, 657]
-console.timeEnd('练习') // 练习: 0.6279296875 ms
+console.timeEnd("练习"); // 练习: 0.6279296875 ms
