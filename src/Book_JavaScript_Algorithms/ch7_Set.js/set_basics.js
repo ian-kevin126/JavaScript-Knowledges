f78bee69ffcb1510f08d36f7abf5aa256442dc7c
liaojiayun <liaojiayun@moresales.cn>
2021-08-31 10:44:24 +0800

    feat: add map

--- src/Book_JavaScript_Algorithms/ch7_Set.js/set_basics.js
+++ src/Book_JavaScript_Algorithms/ch7_Set.js/set_basics.js
@@ -1,7 +1,7 @@
 /**
  * 集合：是由一组无序且唯一（即不能重复）的项组成的数据结构，该数据结构使用了与有限集合相同的数学概念。
  */
-class Set {
+class SetBasic {
   constructor() {
     this.items = {};
   }
@@ -84,7 +84,7 @@ class Set {
 
   // 并集
   union(otherSet) {
-    const unionSet = new Set();
+    const unionSet = new SetBasic();
     this.values().forEach((element) => unionSet.add(element));
     otherSet.values().forEach((element) => unionSet.add(element));
     return unionSet;
@@ -92,7 +92,7 @@ class Set {
 
   // 交集
   intersection(otherSet) {
-    const intersectionSet = new Set();
+    const intersectionSet = new SetBasic();
     this.values().forEach((element) => {
       if (otherSet.has(element)) {
         intersectionSet.add(element);
@@ -103,7 +103,7 @@ class Set {
 
   // 差集
   difference(otherSet) {
-    const diffSet = new Set();
+    const diffSet = new SetBasic();
     this.values().forEach((element) => {
       if (!otherSet.has(element)) {
         diffSet.add(element);
@@ -129,7 +129,7 @@ class Set {
   }
 }
 
-const _set = new Set();
+const _set = new SetBasic();
 _set.add(0);
 _set.add(1);
 _set.add(2);
@@ -143,7 +143,7 @@ console.log(_set.valuesLegacy()); // (6) [0, 1, 2, 3, 4, 5]
 _set.delete(1);
 console.log(_set.valuesLegacy()); // (5) [0, 2, 3, 4, 5]
 
-const set1 = new Set();
+const set1 = new SetBasic();
 set1.add(3);
 set1.add(4);
 set1.add(5);
@@ -155,10 +155,65 @@ console.log(_set.union(set1).values()); // [0, 2, 3, 4, 5, 6, 7, 8]
 console.log(_set.intersection(set1).values()); // [3, 4, 5]
 console.log(_set.difference(set1).values()); // [0, 2]
 
-const set2 = new Set();
+const set2 = new SetBasic();
 set1.add(3);
 set1.add(4);
 set1.add(5);
 
 console.log(set2.isSubsetOf(set1)); // true
 console.log(_set.isSubsetOf(set1)); // false
+
+/**
+ * ES6新增了一个原生的Set类作为JavaScript API的一部分。
+ */
+const set3 = new Set();
+set3.add(0);
+set3.add(1);
+set3.add(2);
+set3.add(3);
+console.log(set3.values()); // SetIterator {0, 1, 2, 3}
+console.log(set3.has(4));
+set3.delete(1);
+console.log(set3.size);
+console.log(set3.values()); // SetIterator {0, 2, 3}
+
+// 模拟并集
+const union = (setA, setB) => {
+  const unionAb = new Set();
+  setA.forEach((value) => unionAb.add(value));
+  setB.forEach((value) => unionAb.add(value));
+  return unionAb;
+};
+console.log(union(setA, setB));
+
+// 模拟交集
+const intersection = (setA, setB) => {
+  const intersectionSet = new Set();
+  setA.forEach((value) => {
+    if (setB.has(value)) {
+      intersectionSet.add(value);
+    }
+  });
+  return intersectionSet;
+};
+console.log(intersection(setA, setB));
+
+// 模拟差集
+const difference = (setA, setB) => {
+  const differenceSet = new Set();
+  setA.forEach((value) => {
+    if (!setB.has(value)) {
+      // {1}
+      differenceSet.add(value);
+    }
+  });
+  return differenceSet;
+};
+console.log(difference(setA, setB));
+
+// 使用扩展运算符实现并集运算
+console.log(new Set([...setA, ...setB]));
+// 使用扩展运算符实现交集运算
+console.log(new Set([...setA].filter((x) => setB.has(x))));
+// 使用扩展运算符实现差集运算
+console.log(new Set([...setA].filter((x) => !setB.has(x))));
