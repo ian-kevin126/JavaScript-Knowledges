942f7fb2ea32531b452edc9d128f71dec602b6ff
liaojiayun <liaojiayun@moresales.cn>
2021-09-01 09:54:23 +0800

    feat: add avl_tree

--- src/Book_JavaScript_Algorithms/ch10_tree/avl_tree.js
+++ src/Book_JavaScript_Algorithms/ch10_tree/avl_tree.js
@@ -39,7 +39,7 @@ class AVLTree extends BinarySearchTree {
   }
 
   /**
-   * 左-左（LL）：向右的单旋转，这种情况出现于节点的左侧子节点的高度大于右侧子节点的高度时，并且左侧子节点也是平衡或左侧较重的。
+   * 左-左（LL）：向右的单旋转——这种情况出现于节点的左侧子节点的高度大于右侧子节点的高度时，并且左侧子节点也是平衡或左侧较重的。
    *
    *       b                           a
    *      / \                         / \
@@ -49,46 +49,72 @@ class AVLTree extends BinarySearchTree {
    * @param node Node<T>
    */
   rotationLL(node) {
-    // 与平衡相关的三个节点是a、b、c码匠节点
+    // 1，与平衡相关的三个节点是a、b、c，将节点a置于节点b的位置，a的左子树保持不变，tmp就是a节点，node就是b节点
     const tmp = node.left;
+    // 2，然后将节点a子节点中最大的键（右子节点）移动到b的左子树上
     node.left = tmp.right;
+    // 3，再重新将a的右子节点重置为b
     tmp.right = node;
     return tmp;
   }
 
   /**
-   * 右-右（RR）：向左的单旋转
+   * 右-右（RR）：向左的单旋转——这种情况出现于右侧子节点的高度大于左侧子节点的高度，并且右侧子节点也是平衡或者右侧较重的。与左-左相反
    *
    *     a                              b
    *    / \                            / \
    *   c   b   -> rotationRR(a) ->    a   e
    *      / \                        / \
    *     d   e                      c   d
-   *
    * @param node Node<T>
    */
   rotationRR(node) {
+    // 1，与平衡相关的节点有三个a，b，e，将节点b置于节点a的位置，tmp是节点b，node是a
     const tmp = node.right;
+    // 2，将节点b子节点中最小的键（左子节点）移动到节点a的右子节点上
     node.right = tmp.left;
+    // 3，再重新将b的左子节点重置为a
     tmp.left = node;
     return tmp;
   }
 
   /**
-   * 左-右（LR）：向右的双旋转（先 LL 旋转，再 RR 旋转）
+   * 左-右（LR）：向右的双旋转（先 LL 旋转，再 RR 旋转）——这种情况出现于左侧子节点的高度大于右侧子节点的高度，并且左侧子节点右侧较重（因此称之为左-右）。
+   *
+   *       b                           b
+   *      / \                         / \
+   *     a   e -> rotationLL(a) ->   d   e   -> rotationRR(a) ->      d
+   *    / \                         / \                             /   \
+   *   c   d                       a   g                           a     b
+   *      / \                     / \                             / \   / \
+   *     f   g                   c   f                           c   f g   e
+   *
    * @param node Node<T>
    */
   rotationLR(node) {
+    // 1，先做一次LL旋转
     node.left = this.rotationRR(node.left);
+    // 2，再做一次RR旋转
     return this.rotationLL(node);
   }
 
   /**
-   * 右-左（RL）：向左的双旋转（先 RR 旋转，再 LL 旋转）
+   * 右-左（RL）：向左的双旋转（先 RR 旋转，再 LL 旋转）——这种情况和做-右相反，出现于右侧子节点的高度大于左侧子节点的高度，并且右侧子节点左侧较重（因此称为右-左）。
+   *
+   *     a                              a
+   *    / \                            / \
+   *   c   b   -> rotationRR(b) ->    c   d   -> rotationLL(a) ->       d
+   *      / \                            / \                          /   \
+   *     d   e                          f   b                        a     b
+   *    / \                                / \                      / \   / \
+   *   f   g                              g   e                    c   f g   e
+   *
    * @param node Node<T>
    */
   rotationRL(node) {
+    // 1，先做一次RR旋转
     node.right = this.rotationLL(node.right);
+    // 2，再做一次LL旋转
     return this.rotationRR(node);
   }
 
@@ -114,11 +140,14 @@ class AVLTree extends BinarySearchTree {
     }
   }
 
+  // 向AVL树插入节点
   insert(key) {
     this.root = this.insertNode(this.root, key);
   }
 
+  // 向AVL树插入节点和在BST中是一样的，除了插入节点外，我们还要验证插入后的树是否还是平衡的，如果不是，就要进行比较的旋转操作。
   insertNode(node, key) {
+    // 插入操作和BST是一样的
     if (node == null) {
       return new Node(key);
     } else if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
@@ -126,25 +155,28 @@ class AVLTree extends BinarySearchTree {
     } else if (this.compareFn(key, node.key) === Compare.BIGGER_THAN) {
       node.right = this.insertNode(node.right, key);
     } else {
-      return node; // duplicated key
+      return node; // 重复的键
     }
-    // verify if tree is balanced
+
+    // 检查树是否平衡，因此要使用递归计算每个插入树的节点为根的平衡因子，然后对每种情况应用正确的旋转。
     const balanceFactor = this.getBalanceFactor(node);
     if (balanceFactor === BalanceFactor.UNBALANCED_LEFT) {
+      // 1，如果在向左子树插入节点后不平衡了，我们需要比较是否插入的键小于左侧子节点的键
       if (this.compareFn(key, node.left.key) === Compare.LESS_THAN) {
-        // 左-左（LL）：向右的单旋转
+        // （1）如果是，则进行左-左（LL）：向右的单旋转
         node = this.rotationLL(node);
       } else {
-        // 左-右（LR）：向右的双旋转（先 LL 旋转，再 RR 旋转）
+        // （2）如果不是，进行左-右（LR）：向右的双旋转（先 LL 旋转，再 RR 旋转）
         return this.rotationLR(node);
       }
     }
     if (balanceFactor === BalanceFactor.UNBALANCED_RIGHT) {
+      // 2，如果在向右子树插入节点后不平衡了，我们需要比较是否插入的键大于右侧子节点的键
       if (this.compareFn(key, node.right.key) === Compare.BIGGER_THAN) {
-        // 右-右（RR）：向左的单旋转
+        // （1）如果是，则进行右-右（RR）：向左的单旋转
         node = this.rotationRR(node);
       } else {
-        // 右-左（RL）：向左的双旋转（先 RR 旋转，再 LL 旋转）
+        // （2）如果不是，则进行右-左（RL）：向左的双旋转（先 RR 旋转，再 LL 旋转）
         return this.rotationRL(node);
       }
     }
